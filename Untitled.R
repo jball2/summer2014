11a337e67b085c55341b0bc3cd961e79656107c7
Juliet Pulliam <pulliam@ufl.edu>
2014-07-01 16:39:41 -0400

    put ymax calc in right place

--- Untitled.R
+++ Untitled.R
@@ -11,10 +11,10 @@ y0 <- c(S=500000-2250, I=2250)
 parmsA <- c(N=500000, L=4, D= 0.02, b0=500, b1=0.02)
 parmsB <- c(N=500000, L=8, D= 0.025, b0=400, b1=0.02)
 
-ymax <- 2*mean(out[which(out[,'time'] > 10),'I'])
 
 plotter<-function(parms) {
   out <- ode(y0, seq(0,20,0.1), f, parms)
+  ymax <- 2*mean(out[which(out[,'time'] > 10),'I'])
   plot(out[,'time'], out[,'I'], type = "l", xlim=c(10,20), ylim=c(0,ymax), ylab="I", xlab="year")
 }
 
